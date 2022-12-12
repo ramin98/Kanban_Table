c56f19a8b3ef538291b47cf40eed5af8cb661ea1
ramin98 <59912261+ramin98@users.noreply.github.com>
2022-12-12 14:17:58 +0400

    Update Tables.jsx

--- src/Canban/Tables/Tables.jsx
+++ src/Canban/Tables/Tables.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Item from "./components/Item";
 import DropWrapper from "./components/DropWrapper";
 import Col from "./components/Col";
